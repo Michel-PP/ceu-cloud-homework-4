39cbaa3676782e84a1a8d898af0723088a8daf46
Michel Poupa <84421995+Michel-PP@users.noreply.github.com>
2024-12-22 11:40:07 +0100

    Update gold_allviews.sql

--- gold_allviews.sql
+++ gold_allviews.sql
@@ -1,2 +1,15 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `gold_allviews` TABLE
- 
+DROP TABLE IF EXISTS michel_pp_homework.gold_allviews;
+
+CREATE TABLE michel_pp_homework.gold_allviews
+    WITH (
+          format = 'PARQUET',
+          parquet_compression = 'SNAPPY',
+          external_location = 's3://michel-cc-wikidata/datalake/gold_allviews/'
+    ) AS
+    SELECT 
+        article,
+        SUM(views) AS total_top_view,
+        MIN(rank) AS top_rank,
+        COUNT(DISTINCT date) AS ranked_days
+    FROM michel_pp_homework.silver_views
+    GROUP BY article;
