0a189f173efc973815fd3a657b7007f8ac128335
Michel Poupa <84421995+Michel-PP@users.noreply.github.com>
2024-12-22 11:37:36 +0100

    Update bronze_views.sql

--- bronze_views.sql
+++ bronze_views.sql
@@ -1,2 +1,11 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `bronze_views` TABLE
- 
+DROP TABLE IF EXISTS michel_pp_homework.bronze_views;
+
+CREATE EXTERNAL TABLE
+michel_pp_homework.bronze_views (
+    article STRING,
+    views INT,
+    rank INT,
+    date DATE,
+    retrieved_at STRING) 
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://michel-cc-wikidata/datalake/views/';
