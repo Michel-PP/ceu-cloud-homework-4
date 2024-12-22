23a5ebfeb303a1306f2441842218f46d0641bd9f
Michel Poupa <84421995+Michel-PP@users.noreply.github.com>
2024-12-22 11:39:37 +0100

    Update silver_views.sql

--- silver_views.sql
+++ silver_views.sql
@@ -1,2 +1,14 @@
--- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `silver_views` TABLE
- 
+DROP TABLE IF EXISTS michel_pp_homework.silver_views
+
+CREATE TABLE michel_pp_homework.silver_views
+    WITH (
+          format = 'PARQUET',
+          parquet_compression = 'SNAPPY',
+          external_location = 's3://michel-cc-wikidata/datalake/views_silver/'
+    ) AS
+    SELECT 
+        article,
+        views,
+        rank,
+        date
+    FROM michel_pp_homework.bronze_views
